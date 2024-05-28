4689bdd6c93da225c7c34e02866a55534b1a9310
cr4sh-me <cr4sh.lab@gmail.com>
2024-05-28 17:33:54 +0200

    INIT

---
+++ setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+APKTOOL_VERSION="2.9.3"
+APKTOOL_FILENAME="apktool_$APKTOOL_VERSION.jar"
+
+rm -f $APKTOOL_FILENAME
+wget https://bitbucket.org/iBotPeaches/apktool/downloads/$APKTOOL_FILENAME
+
+echo '#!/usr/bin/bash' > /opt/cybertools/bin/apktool
+echo "java -jar /opt/cybertools/apktool/$APKTOOL_FILENAME \"\$@\"" >> /opt/cybertools/bin/apktool
+chmod +x /opt/cybertools/bin/apktool
